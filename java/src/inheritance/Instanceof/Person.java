6861ee21d79c4583f78dacf96b55c2da10ecdc19
kimsungyun11 <980808ksy@gmail.com>
2024-07-31 17:28:06 +0900

    자바

---
+++ java/src/inheritance/Instanceof/Person.java
@@ -0,0 +1,17 @@
+package inheritance.Instanceof;
+
+public class Person {
+	
+	// 필드 선언
+	public String name;
+	
+	// 생성자 선언
+	public Person ( String name ) {
+		this.name = name;
+	}
+	
+	// 메소드 선언
+	public void walk() {
+		System.out.println( "걷다" );
+	}
+}
